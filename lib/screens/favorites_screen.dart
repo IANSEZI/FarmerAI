314af4004e909668cf0eb33b59a534b78f741bde
MUGERWAIBRAHIM <mugerwaibrahim90@gmail.com>
2025-07-30 10:30:50 +0300

    Update favorites_screen.dart

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -15,7 +15,7 @@ class FavoritesScreen extends StatelessWidget {
       'details':
           'Heavy rains are forecasted in the western region next week...',
     },
-  ];
+  ];//
 
   @override
   Widget build(BuildContext context) {
